0e245ae3c3c080d5754bd2aea7afe74391b5d61c
ALSephirot <asephirotammale@gmail.com>
2015-06-11 18:18:25 -0500

    Quienes Somos

--- js/QuienesSomos.js
+++ js/QuienesSomos.js
@@ -15,11 +15,30 @@ function CargarSlider (ObjectInfo) {
 }
 
 function CargarEmpleados (Empleados) {
-	// body...
+	var HTMLEmpleados = "";
+
+	$.each(Empleados, function(index, item) {
+		HTMLEmpleados +='<div class="ContenedorPpalQS">'+
+					'<figure class="ImagenEmpleado">'+
+						'<img src="img/'+ item.id +'.png" alt="Usuario">'+
+					'</figure>'+
+					'<div class="NombreEmpleado">'+
+						'<p>'+ item.Nombre +'</p>'+
+						'<p>'+ item.Cargo +'</p>'+
+					'</div>'+
+					'<div class="ContentRedes">'+
+						'<figure><img id="EmpleadoFacebook" src="img/facebook.png" alt="Facebook" data-src="'+ item.Facebook +'" onclick="ClickRedesEmpleado(this)"></figure>'+
+						'<figure><img id="EmpleadoTwitter" src="img/twitter.png" alt="Twitter" data-src="'+ item.Twitter +'" onclick="ClickRedesEmpleado(this)"></figure>'+
+						'<figure><img id="EmpleadoLinkid" src="img/linkid.png"  alt="LinkId" data-src="'+ item.LinkId +'" onclick="ClickRedesEmpleado(this)"></figure>'+
+					'</div>'+
+					'<p class="DescripcionEmpleado">'+ item.Descripcion +'</p>'+
+
+				'</div>';	 
+	});
 }
 
 function ClickRedesEmpleado(red) {
-	// body...
+	window.open($(red).attr("data-src"),"_blank");
 }
 
 $(document).ready(function() {
